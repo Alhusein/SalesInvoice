0f233675629849ad04fb6ef73ad8a1e9dfd49598
Alhusein <114861673+Alhusein@users.noreply.github.com>
2022-11-25 19:22:42 +0200

    add code

---
+++ src/Model/Invoice.java
@@ -0,0 +1,77 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Al-Hussein
+ */
+public class Invoice {
+    private int num;
+    private String date;
+    private String customer;
+    private ArrayList<Line> lines;
+    
+    public Invoice() {
+    }
+
+    public Invoice(int num, String date, String customer) {
+        this.num = num;
+        this.date = date;
+        this.customer = customer;
+    }
+
+    public double getInvoiceTotal() {
+        double total = 0.0;
+        for (Line line : getLines()) {
+            total += line.getLineTotal();
+        }
+        return total;
+    }
+    
+    public ArrayList<Line> getLines() {
+        if (lines == null) {
+            lines = new ArrayList<>();
+        }
+        return lines;
+    }
+
+    public String getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(String customer) {
+        this.customer = customer;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "Invoice{" + "num=" + num + ", date=" + date + ", customer=" + customer + '}';
+    }
+    
+    public String getAsCSV() {
+        return num + "," + date + "," + customer;
+    }
+    
+}
+
